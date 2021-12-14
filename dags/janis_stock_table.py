8cd75a5fc718fa6489e7a7d9b4d36194afb45892
iurizar-smu <iurizar@smu.cl>
2021-12-14 14:16:34 -0300

    Remove unused import

--- dags/janis_stock_table.py
+++ dags/janis_stock_table.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.models import Variable
-from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.operators.python import PythonOperator
 
 from utils.janis_utils import load_full_table_to_s3
 
