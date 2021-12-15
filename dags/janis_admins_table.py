89f4ed5d1c490d76076e621c43fdb7da15863831
iurizar-smu <iurizar@smu.cl>
2021-12-15 10:18:09 -0300

    Create janis_admins_full_load dag

---
+++ dags/janis_admins_table.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+
+from utils.janis_utils import load_full_table_to_s3
+
+from datetime import datetime
+from io import StringIO
+
+import boto3
+import botocore
+import mysql.connector
+import pandas as pd
+import psycopg2
+import sqlalchemy
+
+default_args = {
+    "owner": "ecommerce_data",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+}
+with DAG(
+    'janis_admins_full_table_load',
+    default_args=default_args,
+    description="Extracción y carga de tabla admins desde Janis Replica.",
+    schedule_interval="0 7 * * *",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["DATA", "Janis"],
+) as dag:
+
+    dag.doc_md = """
+    Extracción y carga de tabla de admins de Janis.
+    """ 
+    t0 = PythonOperator(
+        task_id = "load_full_table_to_s3",
+        python_callable = load_full_table_to_s3,
+        op_kwargs = {"table_name": "admins"}
+    )
+
+    t0
