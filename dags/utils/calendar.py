20e15da5722122a3399e8c5a4987f883a9467100
iurizar-smu <iurizar@smu.cl>
2022-01-10 09:16:58 -0300

    Fix week 53 validation

--- dags/utils/calendar.py
+++ dags/utils/calendar.py
@@ -1,7 +1,5 @@
 
 def delta_yearweeks(comp_year, base_year, years_53):
-    print(base_year)
-    print(comp_year)
     delta = (comp_year - base_year)*52
     if base_year > comp_year:
         start_year = comp_year
