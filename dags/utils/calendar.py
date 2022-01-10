10ba82fd0c654775d0b608f9403d81b84ccf3766
iurizar-smu <iurizar@smu.cl>
2022-01-10 08:47:14 -0300

    Fix relative week calculation

--- dags/utils/calendar.py
+++ dags/utils/calendar.py
@@ -1,9 +1,8 @@
 
-def delta_yearweeks(base_year, comp_year, years_53):
+def delta_yearweeks(comp_year, base_year, years_53):
     print(base_year)
     print(comp_year)
     delta = (comp_year - base_year)*52
-    print("OK")
     if base_year > comp_year:
         start_year = comp_year
         end_year = base_year
@@ -11,8 +10,6 @@ def delta_yearweeks(base_year, comp_year, years_53):
         start_year = base_year
         end_year = comp_year
     for year in range(start_year, end_year):
-        print(f"year: {year}")
         if year in years_53:
-            print("YES")
             delta = delta + 1 if delta > 0 else delta - 1 
     return delta
