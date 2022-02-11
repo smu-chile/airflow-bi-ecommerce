2d80dbe119a97cdc43463e31339f83298c83b93c
iurizar-smu <iurizar@smu.cl>
2022-02-11 09:40:19 -0300

    Add unixtime parameter to method call

--- dags/etl_orders_incremental_load.py
+++ dags/etl_orders_incremental_load.py
@@ -256,7 +256,8 @@ with DAG(
         op_kwargs = {
             "table_name": "wms_orders", 
             "xcom_updated_date_task_id": "get_max_updated_at_date", 
-            "updated_column": "date_modified"
+            "updated_column": "date_modified",
+            "from_unixtime": True
         }
     )
 
