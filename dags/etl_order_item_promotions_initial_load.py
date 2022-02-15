e9170cffb59a3a87fb8dfe221e96db643af9f108
iurizar-smu <iurizar@smu.cl>
2022-02-15 14:55:10 -0300

    Create order_item_promotions initial load etl

---
+++ dags/etl_order_item_promotions_initial_load.py
@@ -0,0 +1,123 @@
+from airflow import DAG
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+
+from utils.janis_utils import load_custom_query_to_s3
+
+from datetime import datetime
+
+def _create_initial_order_item_promotions_table(ti):
+    import numpy as np
+    import pandas as pd
+    import sqlalchemy
+    from sqlalchemy import text
+    
+    order_item_proms_file = ti.xcom_pull(key="return_value", task_ids=["load_full_table_to_s3"])[0]
+
+    s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
+    s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
+
+    print("Searching file: "+order_item_proms_file)
+    if not s3_hook.check_for_key(order_item_proms_file, bucket_name=s3_bucket):
+        raise Exception("Key %s does not exist." % order_item_proms_file)
+
+    order_item_proms_object = s3_hook.get_key(order_item_proms_file, bucket_name=s3_bucket)
+
+    df = pd.read_csv(order_item_proms_object.get()["Body"])
+    df = df[[
+        "id", 
+        "order_item", 
+        "name", 
+        "quantity", 
+        "value" 
+    ]]  
+
+    # # Ensure correct datatypes:
+    df["id"] = df["id"].astype("int")
+    df["order_item"] = df["order_item"].astype("int")
+    df["name"] = df["name"].astype("str", errors="ignore")
+    df["quantity"] = df["quantity"].astype("int", errors="ignore")
+    df["value"] = df["value"].astype("float", errors="ignore")
+
+    columns_rename = {
+        "order_item": "orden_producto",
+        "name": "nombre",
+        "quantity": "cantidad",
+        "value": "valor"
+    }
+
+    df = df.rename(columns=columns_rename)
+
+    print("Number of records to be loaded: "+str(len(df.index)))
+
+    host = Variable.get("POSTGRESQL_HOST")
+    database = Variable.get("POSTGRESQL_DB")
+    username = Variable.get("POSTGRESQL_USER")
+    password = Variable.get("POSTGRESQL_PASSWORD")
+    
+    conn_url = "postgresql+psycopg2://"+username+":"+password+"@"+host+":5432/"+database
+    engine = sqlalchemy.create_engine(conn_url)
+
+    connection = engine.connect()
+    truncate_query = "TRUNCATE TABLE ecommdata.orden_producto_promociones"
+    connection.execute(text(truncate_query))
+    connection.close()
+
+    # Save to PostgreSQL:
+    df.to_sql(name="orden_producto_promociones",
+                con=engine,         
+                schema="ecommdata",         
+                if_exists='append',         
+                index=False,         
+                chunksize=20000,         
+                method='multi')
+
+    print("Data saved to PostgreSQL. Table: ecommdata.orden_producto_promociones")
+
+    return
+
+default_args = {
+    "owner": "ecommerce_data",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+}
+with DAG(
+    'etl_orden_producto_promociones_initial_load',
+    default_args=default_args,
+    description="Extracción y carga inicial de tabla orden_producto_promociones desde Janis Replica hasta Workspace.",
+    schedule_interval=None,
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=["DATA", "Janis", "ecommdata", "orden_producto_promociones"],
+) as dag:
+
+    dag.doc_md = """
+    Extracción y carga inicial de tabla de orden_producto_promociones de Janis.
+    """ 
+
+    t0 = PythonOperator(
+        task_id = "load_full_table_to_s3",
+        python_callable = load_custom_query_to_s3,
+        op_kwargs = {
+            "query": """
+                SELECT woip.*
+                FROM janis_jackie.wms_orders AS wo
+                JOIN janis_jackie.wms_order_items AS woi
+                ON woi.order_id = wo.id
+                JOIN janis_jackie.wms_order_item_promotions AS woip
+                ON woip.order_item = woi.id
+                WHERE wo.id >= 168886
+            """,
+            "query_name": "wms_order_item_promotions",
+        }
+    )
+
+    t1 = PythonOperator(
+        task_id = "create_initial_orden_producto_promociones_table",
+        python_callable = _create_initial_order_item_promotions_table
+    )
+
+    t0 >> t1
