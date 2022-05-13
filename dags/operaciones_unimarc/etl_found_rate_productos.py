34206415056dcef2d39da13e9b0ae2439b4c92fd
mseguraSMU <mseguraa@smu.cl>
2022-05-13 10:54:24 -0400

    Name Changes
    changed names to DAG and sql to found_rate_productos

--- dags/operaciones_unimarc/etl_found_rate_productos.py
+++ dags/operaciones_unimarc/etl_found_rate_productos.py
@@ -14,29 +14,29 @@ default_args = {
     "retries": 0,
 }
 with DAG(
-    'foundrate',
+    'etl_found_rate_productos_unimarc',
     default_args=default_args,
-    description="Carga de tabla foundrate",
+    description="Carga de tabla found_rate_productos",
     schedule_interval="35 * * * *",
-    start_date=datetime(2022, 5, 9),
+    start_date=datetime(2021, 9, 1),
     catchup=True,
-    tags=["DATA", "foundrate", "operaciones_unimarc"],
+    tags=["DATA", "found_rate_productos", "operaciones_unimarc", "unimarc"],
 ) as dag:
 
     dag.doc_md = """
-    Carga de tabla foundrate. El resultado final queda en datamart operaciones_unimarc.
+    Carga de tabla found_rate_productos. El resultado final queda en datamart operaciones_unimarc.
     """ 
     t0 = PostgresOperator(
         task_id = "load_table_foundrate",
         postgres_conn_id="postgresql_conn",
-        sql="sql/foundrate.sql",
+        sql="sql/found_rate_productos.sql",
     )
 
     t1 = PostgresOperator(
         task_id = "delete_old_data",
         postgres_conn_id="postgresql_conn",
         sql="""
-        DELETE from operaciones_unimarc.found_rate
+        DELETE from operaciones_unimarc.found_rate_productos
         WHERE fecha_facturacion <= to_date('{{execution_date.strftime('%Y-%m-%d')}}', '%YYYY-%mm-%dd') - interval '24 months'
         """,
     )
