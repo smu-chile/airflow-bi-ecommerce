e7349da6856e66cf33cb60a5962e5a8ebfdfa52a
iurizar-smu <iurizar@smu.cl>
2022-05-25 15:35:28 -0400

    Order columns before upsert

--- dags/analytics_and_growth/etl_tickets_zendesk_incremental_load.py
+++ dags/analytics_and_growth/etl_tickets_zendesk_incremental_load.py
@@ -99,6 +99,7 @@ def _load_tickets_zendesk(ts):
         "total_dias_hasta_resolucion" 
     ]
 
+    df = df[columns]
     columns_query = ",".join(columns)
     excluded_query = ",".join(["EXCLUDED."+column for column in columns])
     values_query = "%s,"+",".join(["%s" for column in columns])
