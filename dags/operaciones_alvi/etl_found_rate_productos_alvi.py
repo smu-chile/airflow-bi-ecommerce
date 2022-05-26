c22d92fb6b7bb4eda02cb063dffc0348b5fa3f9e
mseguraSMU <mseguraa@smu.cl>
2022-05-26 12:31:11 -0400

    Update etl_found_rate_productos_alvi.py
    Added dependency to the DAGs ordenes_janis, orden_productos, producto_pesables through multiple ExternalTaskSensor().
    Also changed the schedule_interval to match the upstream DAGs.

--- dags/operaciones_alvi/etl_found_rate_productos_alvi.py
+++ dags/operaciones_alvi/etl_found_rate_productos_alvi.py
@@ -17,7 +17,7 @@ with DAG(
     'etl_found_rate_productos_alvi',
     default_args=default_args,
     description="Carga de tabla found_rate_productos de Alvi",
-    schedule_interval="35 * * * *",
+    schedule_interval="30 * * * *",
     start_date=datetime(2022, 5, 1),
     catchup=True,
     tags=["DATA", "found_rate_productos", "operaciones_alvi", "Alvi"],
@@ -26,13 +26,37 @@ with DAG(
     dag.doc_md = """
     Carga de tabla found_rate_productos de Alvi. El resultado final queda en datamart operaciones_alvi.
     """ 
-    t0 = PostgresOperator(
+    t0 = ExternalTaskSensor(
+        task_id="wait_for_ordenes_janis",
+        external_dag_id='etl_ordenes_janis_alvi_incremental_load',
+        external_task_id=None,
+        allowed_states=['success'],
+        failed_states=['failed', 'skipped']
+    )
+
+    t1 = ExternalTaskSensor(
+        task_id="wait_for_orden_productos",
+        external_dag_id='etl_orden_productos_alvi_incremental_load',
+        external_task_id=None,
+        allowed_states=['success'],
+        failed_states=['failed', 'skipped']
+    )
+
+    t2 = ExternalTaskSensor(
+        task_id="wait_for_orden_producto_pesables",
+        external_dag_id='etl_orden_producto_pesables_alvi_incremental_load',
+        external_task_id=None,
+        allowed_states=['success'],
+        failed_states=['failed', 'skipped']
+    )
+    
+    t3 = PostgresOperator(
         task_id = "load_table_foundrate",
         postgres_conn_id="postgresql_conn",
         sql="sql/found_rate_productos_alvi.sql",
     )
 
-    t1 = PostgresOperator(
+    t4 = PostgresOperator(
         task_id = "delete_old_data",
         postgres_conn_id="postgresql_conn",
         sql="""
@@ -41,4 +65,4 @@ with DAG(
         """,
     )
 
-    t0 >> t1
+    [t0, t1, t2] >> t3 >> t4
