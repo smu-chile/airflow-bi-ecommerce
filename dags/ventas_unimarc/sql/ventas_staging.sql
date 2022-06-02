c9d70f5005d2d6d01b83c751ff340bfe58603e70
iurizar-smu <iurizar@smu.cl>
2022-06-02 14:55:46 -0400

    Add extra or condition

--- dags/ventas_unimarc/sql/ventas_staging.sql
+++ dags/ventas_unimarc/sql/ventas_staging.sql
@@ -23,14 +23,14 @@ select _t.fecha_facturacion
 		, coalesce(_t.costo_unitario_neto,0) as costo_unitario_neto 
 		, coalesce(_t.unidades_pickeadas_original * _t.costo_unitario_neto,0) as cxq_neto
 		, coalesce(_t.pxq_neto - (_t.unidades_pickeadas_original * _t.costo_unitario_neto),0) as contribucion_neta_1
-		, coalesce(sum(case when _t.id_evento <> 400 then _t.ahorro_promocion else 0 end),0) as ahorro_promocion_cadena
+		, coalesce(sum(case when (_t.id_evento <> 400) or (_t.id_evento is null) then _t.ahorro_promocion else 0 end),0) as ahorro_promocion_cadena
 		, coalesce(sum(case when _t.id_evento = 400 then _t.ahorro_promocion else 0 end),0) as ahorro_promocion_ecommerce
 		, 0 as ahorro_promocion_personalizado
 		, coalesce(sum(_t.ahorro_promocion),0) as ahorro_promocion_total
-		, coalesce(sum(case when _t.id_evento <> 400 then _t.importe_negociado_unitario else 0 end),0) as importe_negociado_unitario_cadena
+		, coalesce(sum(case when (_t.id_evento <> 400) or (_t.id_evento is null) _t.importe_negociado_unitario else 0 end),0) as importe_negociado_unitario_cadena
 		, coalesce(sum(case when _t.id_evento = 400 then _t.importe_negociado_unitario else 0 end),0) as importe_negociado_unitario_ecommerce
 		, 0 as importe_negociado_unitario_personalizado
-		, sum(case when _t.tipo_financiamiento = 'SELL OUT' and _t.id_evento <> 400 then _t.pxq_importe_negociado else 0 end) as pxq_importe_negociado_sellout_cadena
+		, sum(case when _t.tipo_financiamiento = 'SELL OUT' and ((_t.id_evento <> 400) or (_t.id_evento is null)) then _t.pxq_importe_negociado else 0 end) as pxq_importe_negociado_sellout_cadena
 		, sum(case when _t.tipo_financiamiento = 'SELL OUT' and _t.id_evento = 400 then _t.pxq_importe_negociado else 0 end) as pxq_importe_negociado_sellout_ecommerce
 		, 0 as pxq_importe_negociado_sellout_personalizado
 		, coalesce(sum(_t.pxq_importe_negociado),0) as pxq_importe_negociado_total
