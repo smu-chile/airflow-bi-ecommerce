476f36707be7a38485b6f6b6b20a65ed88015e49
iurizar-smu <iurizar@smu.cl>
2022-06-03 11:45:01 -0400

    Use canal_distribucion and only double check substitutes

--- dags/ventas_unimarc/sql/ventas.sql
+++ dags/ventas_unimarc/sql/ventas.sql
@@ -26,10 +26,10 @@ select _t2.fecha_facturacion
 		, _t2.ahorro_promocion_cadena
 		, _t2.ahorro_promocion_ecommerce
 		, _t2.ahorro_promocion_total
-		, coalesce(sum(case when (_t2.id_evento <> 400) or (_t2.id_evento is null) then _t2.importe_negociado_unitario_2 else 0 end), _t2.importe_negociado_unitario_cadena) as importe_negociado_unitario_cadena
-		, coalesce(sum(case when _t2.id_evento = 400 then _t2.importe_negociado_unitario_2 else 0 end), _t2.importe_negociado_unitario_ecommerce) as importe_negociado_unitario_ecommerce
-		, coalesce(sum(case when (_t2.id_evento <> 400) or (_t2.id_evento is null) then _t2.pxq_importe_negociado_2 else 0 end) , _t2.pxq_importe_negociado_total) as pxq_importe_negociado_sellout_cadena
-		, coalesce(sum(case when _t2.id_evento = 400 then _t2.pxq_importe_negociado_2 else 0 end) , _t2.pxq_importe_negociado_total) as pxq_importe_negociado_sellout_ecommerce
+		, coalesce(sum(case when (_t2.canal_distribucion <> '70') or (_t2.canal_distribucion is null) then _t2.importe_negociado_unitario_2 else 0 end), _t2.importe_negociado_unitario_cadena) as importe_negociado_unitario_cadena
+		, coalesce(sum(case when _t2.canal_distribucion = '70' then _t2.importe_negociado_unitario_2 else 0 end), _t2.importe_negociado_unitario_ecommerce) as importe_negociado_unitario_ecommerce
+		, coalesce(sum(case when (_t2.canal_distribucion <> '70') or (_t2.canal_distribucion is null) then _t2.pxq_importe_negociado_2 else 0 end) , _t2.pxq_importe_negociado_total) as pxq_importe_negociado_sellout_cadena
+		, coalesce(sum(case when _t2.canal_distribucion = '70' then _t2.pxq_importe_negociado_2 else 0 end) , _t2.pxq_importe_negociado_total) as pxq_importe_negociado_sellout_ecommerce
 		, coalesce(sum(_t2.pxq_importe_negociado_2) , _t2.pxq_importe_negociado_total) as pxq_importe_negociado_total 
 		, coalesce ((_t2.pxq_neto - (_t2.unidades_pickeadas_original * _t2.costo_unitario_neto) + sum(_t2.pxq_importe_negociado_2)), _t2.contribucion_neta_2, 0) as contribucion_neta_2
 from (
@@ -68,19 +68,20 @@ from (
 			, _t.contribucion_neta_2
 			, min(importe_negociado_unitario_2) as importe_negociado_unitario_2
 			, min(pxq_importe_negociado_2) as pxq_importe_negociado_2
-			, _t.id_evento
+			, _t.canal_distribucion
 	from (
 		select vs.*
 			, wp2.importe_negociado as importe_negociado_unitario_2 
 			, wp2.importe_negociado * vs.unidades_pickeadas_original as pxq_importe_negociado_2
 			, wp2.n_promocion 
-			, wp2.id_evento
+			, wp2.canal_distribucion
 		from staging.ventas_unimarc vs 
 		left join ecommdata.workflow_promociones wp2 
 			on split_part(vs.ref_id, '-', 1) = wp2.material 
 			and not vs.wp_promocion2
 			and wp2.tipo_financiamiento = 'SELL OUT'
 			and vs.fecha_facturacion between wp2.fecha_inicio_de_promocion and wp2.fecha_fin_de_promocion and wp2.id_evento not in (102)
+			and vs.producto_substituto
 		where vs.fecha_facturacion = to_date('{{execution_date.strftime('%Y-%m-%d')}}', '%YYYY-%mm-%dd') 
 	) _t
 	group by _t.fecha_facturacion 
@@ -120,7 +121,7 @@ from (
 			, _t.pxq_importe_negociado_sellout_personalizado
 			, _t.contribucion_neta_2
 			, _t.n_promocion
-			, _t.id_evento
+			, _t.canal_distribucion
 	) _t2
 group by fecha_facturacion 
 			, fecha_picking 
