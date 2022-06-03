4add1f63e27aec5749856f3759e2ec8ffc6452b2
mseguraSMU <mseguraa@smu.cl>
2022-06-03 10:20:29 -0400

    Update dm_items_not_found_unimarc_incremental_load.py
    added external task sensor library

--- dags/operaciones_unimarc/dm_items_not_found_unimarc_incremental_load.py
+++ dags/operaciones_unimarc/dm_items_not_found_unimarc_incremental_load.py
@@ -4,6 +4,7 @@ from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.hooks.S3_hook import S3Hook
+from airflow.sensors.external_task import ExternalTaskSensor
 
 from datetime import datetime
 
