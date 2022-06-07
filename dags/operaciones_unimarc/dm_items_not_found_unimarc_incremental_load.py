e0f0e1b4291d22498692795a275ad65aa51ee08a
mseguraSMU <mseguraa@smu.cl>
2022-06-07 12:20:26 -0400

    Update dm_items_not_found_unimarc_incremental_load.py
    changing decimal to float

--- dags/operaciones_unimarc/dm_items_not_found_unimarc_incremental_load.py
+++ dags/operaciones_unimarc/dm_items_not_found_unimarc_incremental_load.py
@@ -50,6 +50,8 @@ def _upsert_table_from_ecommdata_into_DM(ti):
         columns = ['fecha_proceso', 'ref_id', 'ean', 'id_tienda', 'marca', 'ordenes_afectadas', 'unidades_faltantes', 'inicio_bloque', 'fin_bloque', 'fecha_modificacion']
     )
 
+    df['unidades_faltantes'] = df['unidades_faltantes'] * 1.0
+
     print("Number of records to be loaded: "+str(len(df.index)))
 
     host = Variable.get("DM_HOST")
