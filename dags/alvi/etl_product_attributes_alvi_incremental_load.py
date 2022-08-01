78b6aec0bab5cc2fcfdb8873546fe85d347c14a3
mseguraSMU <mseguraa@smu.cl>
2022-08-01 16:14:13 -0400

    external task sensors to product_attributes

--- dags/alvi/etl_product_attributes_alvi_incremental_load.py
+++ dags/alvi/etl_product_attributes_alvi_incremental_load.py
@@ -3,6 +3,7 @@ from airflow.hooks.S3_hook import S3Hook
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.sensors.external_task import ExternalTaskSensor
 
 from utils.janis_alvi_utils import incremental_unixtime_load_table_s3
 from utils.postgres_utils import get_max_updated_at_value
@@ -136,11 +137,14 @@ def _incremental_load_product_attributes_table(ti):
         fixed_records.append(tuple(fixed_record))
     print(f"Number of records to lo.ad: {str(len(fixed_records))}")
     incremental_query = """
-        BEGIN TRANSACTION;
         INSERT INTO ecommdata_alvi.atributos_producto (id,"""+columns_query+""")
         VALUES ("""+values_query+""")
         ON CONFLICT (id)
         DO UPDATE SET ("""+columns_query+""") = ("""+excluded_query+""");
+    """
+    print(incremental_query)
+    update_query = """
+        BEGIN TRANSACTION;
         UPDATE ecommdata_alvi.atributos_producto ap
         SET ref_id = s.ref_id, nombre_producto = p.nombre
         FROM ecommdata_alvi.skus s
@@ -156,11 +160,11 @@ def _incremental_load_product_attributes_table(ti):
         WHERE ap.valor_atributo_id = va.id;
         COMMIT;
     """
-    print(incremental_query)
     pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
     pg_connection = pg_hook.get_conn()
     cursor = pg_connection.cursor()
     cursor.executemany(incremental_query, fixed_records)
+    cursor.execute(update_query)
     pg_connection.commit()
     cursor.close()
     pg_connection.close()
@@ -189,7 +193,24 @@ with DAG(
     Extracción y carga de tabla de atributos producti de Janis Alvi a Workspace. \n
     UPSERT incremental basado en fecha_modificacion_unixtime.
     """ 
-    t0 = PythonOperator(
+
+    t0 = ExternalTaskSensor(
+        task_id="wait_for_atributos",
+        external_dag_id='etl_atributos_alvi_incremental_load',
+        external_task_id=None,
+        allowed_states=['success'],
+        failed_states=['failed']
+    )
+
+    t1 = ExternalTaskSensor(
+        task_id="wait_for_valores_atributo",
+        external_dag_id='etl_valores_atributo_alvi_incremental_load',
+        external_task_id=None,
+        allowed_states=['success'],
+        failed_states=['failed']
+    )
+
+    t2 = PythonOperator(
         task_id = "get_max_updated_at_date",
         python_callable = get_max_updated_at_value,
         op_kwargs = {
@@ -200,7 +221,7 @@ with DAG(
         }
     )
 
-    t1 = PythonOperator(
+    t3 = PythonOperator(
         task_id = "incremental_unixtime_load_table_to_s3",
         python_callable = incremental_unixtime_load_table_s3,
         op_kwargs = {
@@ -210,9 +231,9 @@ with DAG(
         }
     )
 
-    t2 = PythonOperator(
+    t4 = PythonOperator(
         task_id = "incremental_load_product_attributes_table",
         python_callable = _incremental_load_product_attributes_table
     )
 
-    t0 >> t1 >> t2
+    [t0, t1] >> t2 >> t3 >> t4
