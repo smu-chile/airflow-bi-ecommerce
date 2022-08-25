9c6f90b266b99d1c39a13b8621011034267cb212
iurizar-smu <iurizar@smu.cl>
2022-08-25 13:14:33 -0400

    Query postgres and build a pandas DF

--- dags/unimarc/proc_frogmi_stock_alert.py
+++ dags/unimarc/proc_frogmi_stock_alert.py
@@ -1,7 +1,9 @@
 from airflow import DAG
 from airflow.models import Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
-
+x
+from datetime import datetime
 import pendulum
 
 def _get_time_interval(ts):
@@ -9,14 +11,58 @@ def _get_time_interval(ts):
     # 08:00 -  prev_date at 18:00 to curr_date at 08:00 (+14 hrs)
     # 13:00 -  curr_date at 08:00 to curr_date at 13:00 (+5 hrs)
     # 18:00 -  curr_date at 13:00 to curr_date at 18:00 (+5 hrs)
-    print(f"ts: {ts}")
-    current_exec_hour = ts.split("T")[1][:2]
+    exec_datetime = datetime.strptime(ts[:17], "%Y-%m-%dT%H:%M")
+    exec_datetime_utc = pendulum.timezone("utc").convert(exec_datetime)
+    local_tz = pendulum.timezone("America/Santiago")
+    exec_datetime_local = local_tz.convert(exec_datetime_utc)
+    exec_datetime_local = exec_datetime_local.strftime("%Y-%m-%dT%H:%M")
+    print(exec_datetime_local)
+
+    current_exec_hour = exec_datetime_local.split("T")[1][:2]
     if current_exec_hour == "18":
         return "interval '14 hours'"
     else:
         return "interval '5 hours'"
 
+def _pre_payload(id_tienda):
+    if Variable.get("FROGMI_ENV") != "prod":
+        print("WARNING: THIS IS A TEST RUN OF THIS DAG! Change Env Var: FROGMI_ENV to perform a production run.")
+        id_tienda = "93145c22-7f04-4b44-bbdc-505ba33f2dde"
+
+    base_payload = {
+        "data": [
+            {
+                "type": "task_sku",
+                "attributes": {
+                    "name": "FR-Ecomm 2022-08-24_17:00:00",
+                    "template_id": "a6dbc4bd-64e6-4628-bb6b-66902cba3a7e",
+                    "accountable_area_code": "ADMIN_LOCAL",
+                    "stores": [
+                        id_tienda
+                    ],
+                    "start_date": "2022-08-24T00:00:00-04:00",
+                    "end_date": "2022-08-24T20:00:00-04:00",
+                    "notification":[
+                    ],
+                    "instructions": "Prueba API desde python",
+                    "external_id": "1234567",
+                    "external_data": [
+                        {
+                            "main_text": "Texto tarea de prueba API",
+                            "second_text": "$1",
+                            "icon": "info"
+                        }
+                    ],
+                    "products": []
+                }
+            }
+        ]
+    }
+    return base_payload
+
 def _post_request_to_publish_task_endpoint(ts):
+    import pandas as pd
+
     time_interval = _get_time_interval(ts)
     query = f"""
         select *
@@ -46,6 +92,35 @@ def _post_request_to_publish_task_endpoint(ts):
         ; 
     """
     print(query)
+
+    pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
+    pg_connection = pg_hook.get_conn()
+
+    df = pd.read_sql(query, pg_connection)
+
+    print(f"Number of rows found: {len(df.index)}")
+    df["material"] = df["ref_id"].str.split("-").str[0]
+    df = df.groupby("id_tienda").head(5).reset_index().drop(columns=["index"])
+    print(df)
+    print(df.to_records(index=False))
+    tiendas = df["id_tienda"].drop_duplicates().tolist()
+    print(tiendas)
+    payloads = {tienda: _pre_payload(tienda) for tienda in tiendas}
+
+    registros = df.to_records(index=False)
+
+    for registro in registros:
+        r_tienda = registro[1]
+        r_material = registro[5]
+        body = {
+            "product_code": r_material,
+            "place_code": "alerta_repo"
+        }
+        payloads[r_tienda]["data"][0]["attributes"]["products"].append(body)
+
+    # Send payloads to S3
+    print(payloads)
+
     return
 
 default_args = {
