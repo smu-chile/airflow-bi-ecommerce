d5a800b76c18f0f254c0155ddd6982f45c0af7ef
mseguraSMU <mseguraa@smu.cl>
2022-10-24 18:04:01 -0300

    Update stock_final.sql

--- dags/unimarc/sql/stock_final.sql
+++ dags/unimarc/sql/stock_final.sql
@@ -24,7 +24,7 @@ select
 , svu.cantidad_ilimitada as stock_infinito_vtex
 , su.date_published as fecha_publicacion_janis
 , su.date_modified as fecha_modificacion_janis
-, '{{ts}}'::timestamp as ultima_actualizacion
+, '{{ts}}'::timestamp + interval '1 hour' as ultima_actualizacion
 , l.material is not null and l.excluido is false as surtido_ecommerce
 from staging.stock_vtex_unimarc svu
 left join ecommdata.bodegas b on svu.id_warehouse = b.id 
