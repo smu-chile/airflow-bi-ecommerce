9cbeea30f3b37d8c8038530177cf84e17008e1de
fjaquesmu <fjaquea@smu.cl>
2022-11-30 12:18:08 -0300

    Update etl_costos_logisticos_diarios.py

--- dags/unimarc/etl_costos_logisticos_diarios.py
+++ dags/unimarc/etl_costos_logisticos_diarios.py
@@ -128,7 +128,7 @@ def funcion_km_real(fecha_desde):
 
     cur = conn.cursor()
     #WHERE fecha_facturacion >= '{fecha_desde}
-    cur.execute(f"SELECT fecha_facturacion, id_orden as orden, lat, lng, tienda, transportadora, kms_totales as KMs_Totales FROM forecast_and_planning.tabla_kms';")
+    cur.execute(f"SELECT fecha_facturacion, id_orden as orden, lat, lng, tienda, transportadora, kms_totales as KMs_Totales FROM forecast_and_planning.tabla_kms;")
     rows = cur.fetchall()
     columns = cur.description
     conn.close()
