4692f42455f86202a7ac4b5f687abb0e4c7d3510
fjaquesmu <fjaquea@smu.cl>
2023-01-30 14:10:31 -0300

    Update etl_liberaciones_product_orderid_diarios.py
    -Fix column name

--- dags/unimarc/etl_liberaciones_product_orderid_diarios.py
+++ dags/unimarc/etl_liberaciones_product_orderid_diarios.py
@@ -98,7 +98,7 @@ def _liberacion_diara(ts):
         print (str(e)[:3000])
     
     columns_main_release = [
-        'fecha_de_liberacion', 'id_de_operacion_en_mercadopago', 'numero_de_indentificacion',
+        'fecha_de_liberacion', 'id_de_operacion_en_mercado_pago', 'numero_de_indentificacion',
         'tipo_de_registro', 'descripcion', 'monto_neto_acreditado',
         'monto_neto_debitado', 'monto_bruto_de_la_operacion', 'monto_recibido_por_compras_split',
         'comision_de_mercado_pago_o_mercado_libre', 'comision_por_ofrecer_cuotas_sin_interes',
@@ -110,7 +110,7 @@ def _liberacion_diara(ts):
         'id_del_paquete','datos_extra','costo_por_ofrecer_descuento'
     ]
 
-    df_releases = df_releases.rename(columns={'DATE':'fecha_de_liberacion', 'SOURCE_ID':'id_de_operacion_en_mercadopago',
+    df_releases = df_releases.rename(columns={'DATE':'fecha_de_liberacion', 'SOURCE_ID':'id_de_operacion_en_mercado_pago',
     'EXTERNAL_REFERENCE':'numero_de_indentificacion', 'RECORD_TYPE':'tipo_de_registro', 'DESCRIPTION':'descripcion',
     'NET_CREDIT_AMOUNT':'monto_neto_acreditado', 'NET_DEBIT_AMOUNT':'monto_neto_debitado','GROSS_AMOUNT':'monto_bruto_de_la_operacion',
     'SELLER_AMOUNT':'monto_recibido_por_compras_split', 'MP_FEE_AMOUNT':'comision_de_mercado_pago_o_mercado_libre',
