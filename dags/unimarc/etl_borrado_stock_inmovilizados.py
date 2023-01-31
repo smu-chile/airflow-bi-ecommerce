04d82e0339039be5ee849abd6a830796e748faf0
mseguraSMU <mseguraa@smu.cl>
2023-01-31 15:27:38 -0300

    Update etl_borrado_stock_inmovilizados.py

--- dags/unimarc/etl_borrado_stock_inmovilizados.py
+++ dags/unimarc/etl_borrado_stock_inmovilizados.py
@@ -93,7 +93,7 @@ with DAG(
         task_id = "truncate_table_sales_history",
         postgres_conn_id="postgresql_conn",
         sql="""
-            truncate.historia_venta_dw
+            truncate historia_venta_dw
         """
     )
     
