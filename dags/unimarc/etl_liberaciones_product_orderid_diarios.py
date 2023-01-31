8fd6ea95e280f6a4f03db9d5d4b0cf1b2c3361b2
fjaquesmu <fjaquea@smu.cl>
2023-01-31 10:38:22 -0300

    Update etl_liberaciones_product_orderid_diarios.py
    - Fix try 2

--- dags/unimarc/etl_liberaciones_product_orderid_diarios.py
+++ dags/unimarc/etl_liberaciones_product_orderid_diarios.py
@@ -199,7 +199,7 @@ def _orderid_packid_table():
     columns_query = ",".join(columns_main)
     values_query = ",".join(["%s" for column in columns_main])
     df_packid_orderid = df_packid_orderid.fillna("NULL")
-    df_packid_orderid = df_packid_orderid[~df_packid_orderid['pack_id'] == 'nan']
+    df_packid_orderid = df_packid_orderid[df_packid_orderid['pack_id'] != 'nan']
     records = list(df_packid_orderid.to_records(index=False))
 
     fixed_records = []
