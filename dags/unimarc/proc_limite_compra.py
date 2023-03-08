c2bb9d9b49a221f3dfa50484c262c8d4300922d8
nbonillamarin <nbonilla@smu.cl>
2023-03-08 12:54:39 -0300

    Create proc_limite_compra.py

---
+++ dags/unimarc/proc_limite_compra.py
@@ -0,0 +1,124 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime, timedelta
+
+
+def db_get_ref_id_atributos_producto():
+    import pandas as pd
+    import psycopg2
+
+    # Los registros obtengo los id_producto_janis de la tabla atributos_producto cuyo limite de compra (2847610) es NULL
+    print(f"Iniciando obtencion de datos con limite de compra ...")
+    print("Estableciendo conección con postgres db")
+    conn = psycopg2.connect(
+        host=Variable.get("POSTGRESQL_HOST"),
+        user=Variable.get("POSTGRESQL_USER"),
+        password=Variable.get("POSTGRESQL_PASSWORD"),
+        database=Variable.get("POSTGRESQL_DB")
+    )
+    mycursor = conn.cursor()
+    query = """
+        select pro.nombre, pro.ref_id, pro.vtex_id, att.valor
+        from ecommdata.atributos_producto att
+        full outer join ecommdata.productos pro
+            on pro.id = att.id_producto_janis
+        where att.id_atributo = 2847610
+            and (att.valor not in ('12','11','10','9','8','7','6','5','4','3','2','1','12.0','17','1.0') or att.valor is null);
+        """
+
+    mycursor.execute(query)
+    results = mycursor.fetchall()
+    columns_name = [i[0] for i in mycursor.description]
+    DF_atributos_producto_null = pd.DataFrame(results, columns=columns_name)
+    return list(DF_atributos_producto_null['ref_id'])
+
+
+def set_lim_compra(ti):
+    import json
+    import requests
+
+    lista_ref_id = ti.xcom_pull(
+        task_ids=["db_get_ref_id_atributos_producto"])[0]
+    if len(lista_ref_id) == 0:
+        print("No hay productos para cambiar, las tareas han terminado con exito")
+        return
+
+    headers = {
+        "janis-api-key": Variable.get("JANIS_API_KEY"),
+        "janis-api-secret": Variable.get("JANIS_API_SECRET"),
+        "janis-client": Variable.get("JANIS_CLIENT"),
+        "Connection": "keep-alive"
+    }
+
+    # Creación de big-json
+    jst = []
+    for x in lista_ref_id:
+        item = {"item_id": str(x), "attributes": [
+            {"id": "409", "values": ["12"]}]}
+        jst.append(item)
+    jst = json.dumps(jst)
+
+    # Partición de big-json
+    lim_json = 500
+    if len(jst) > 500:
+        jst = [json.dumps(jst[i:i+lim_json], indent=2)
+               for i in range(0, len(jst), lim_json)]
+        total_size = sum(len(lista) for lista in jst)
+
+    # Datos de conexión
+    session = requests.Session()
+    session.headers.update(headers)
+    API_JANIS = Variable.get("JANIS_API_URL")
+
+    # Seteo vía API al atriubuto limite de compra de la lista de refid
+    r_status = set()
+    cargado = 0
+    for i, jsonString in enumerate(jst, start=1):
+        # Particion
+        r = requests.post(f'{API_JANIS}attribute_value', data=jsonString)
+        r_status.add(str(r.status_code))
+        cargado = cargado + (len(jsonString))
+        print(
+            f"Productos actualizados: {cargado} de {total_size} | Status_Code: {r.content}")
+        print(f"carga finalizada con resultados: {r_status}")
+
+    return r_status
+
+
+default_args = {
+    "owner": "ecommerce_data",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+}
+
+with DAG(
+    'proc_categoria_productos_sustitutos',
+    default_args=default_args,
+    description="Obtención de productos que entran y sale de la categoría Sustitutos",
+    schedule_interval="0 10 * * *",
+    start_date=datetime(2023, 3, 8),
+    catchup=False,
+    max_active_runs=1,
+    tags=["limite_compra", "ecommdata_unimarc", "atributos_producto"],
+) as dag:
+
+    dag.doc_md = """
+        Setea en 12 el valor del atributo "Limite de Compra" cuyos productos tienen valor = NULL 
+        """
+
+    t0 = PythonOperator(
+        task_id='db_get_ref_id_atributos_producto',
+        python_callable=db_get_ref_id_atributos_producto
+    )
+
+    t1 = PythonOperator(
+        task_id='set_lim_compra',
+        python_callable=set_lim_compra
+    )
+
+    t0 >> t1
