a9081ec2291684f7dbc658745fc6865246aa64d7
iurizar-smu <iurizar@smu.cl>
2023-03-09 17:25:38 -0300

    Revert "Remove id_mecanica 26 from query restriction"
    This reverts commit ce0148b635d2b272176460e2aac0d598dfb13cfb.

--- dags/integrations/sql/insert_stock_prices_promos.sql
+++ dags/integrations/sql/insert_stock_prices_promos.sql
@@ -55,7 +55,7 @@ left join (
     and wp.registro_valido = True
     and wp.organizacion_ventas = '1000'
     and wp.canal_distribucion = '10'
-    and wp.id_mecanica not in (25, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99)
+    and wp.id_mecanica not in (25, 26, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99)
     group by wp.ean
 ) _t3
 on _t.ean = _t3.ean
