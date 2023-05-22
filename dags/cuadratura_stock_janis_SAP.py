72844f0200f046ad5d870d2d0a9b674aa4396ac1
Patricio Mardones Gálvez <pmardonesg@smu.cl>
2023-05-22 14:59:42 -0400

    Update cuadratura_stock_janis_SAP.py
    update defs

--- dags/cuadratura_stock_janis_SAP.py
+++ dags/cuadratura_stock_janis_SAP.py
@@ -8,22 +8,9 @@ from airflow.models import Variable
 import pendulum
 from datetime import datetime, timedelta
 
-def stock_lista8():
-    host = "bi-ecommerce-postgres-prod-master.cuuchupawrpt.us-east-1.rds.amazonaws.com"
-    database = "postgres"
-    username = "pmardonesg"
-    password = "GOkh_noaql"
-    conn_url = "postgresql+psycopg2://"+username+":"+password+"@"+host+":5432/"+database
-    engine = sqlalchemy.create_engine(conn_url)
-
-    conn = psycopg2.connect(database=database,
-                            host=host,
-                            user=username,
-                            password=password,
-                            port=5432)
-
-    cursor = conn.cursor()
-    cursor.execute("""select _t.*
+def stock_lista8(ds):
+    import pandas as pd
+    stock_tiendas_query = """select _t.*
                     from( 
                     select s.fecha,
                     s.ref_id,
@@ -39,7 +26,8 @@ def stock_lista8():
                     from ecommdata.lista8 as l
                     inner Join ecommdata.stock as s
                     on l.fecha = s.fecha and l.id_tienda = s.id_tienda and s.ref_id = CONCAT(LPAD(l.material, 18, '0'), '-', l.umv)  
-                    where l.fecha = current_date and l.umv <> 'PAQ') as _t 
+                    where l.fecha = """+ds+"""
+                    and l.umv <> 'PAQ') as _t 
                     group by 
                     _t.fecha,
                     _t.ref_id,
@@ -47,65 +35,40 @@ def stock_lista8():
                     _t.stock_x_umv,
                     _t.stock_janis,
                     _t.stock_sap,
-                    _t.multiplicador_unidad_medida""")
-    df_base = cursor.fetchall()
-    df_base=pd.DataFrame(df_base)
-    df_base.columns = ["fecha","ref_id","id_tienda","stock_l8","stock_janis","stock_calculado","multiplicador_medida"]
-    #conn.close()
-    
-    return df_base
+                    _t.multiplicador_unidad_medida"""
+    pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
+    #print(stock_tiendas_query)
+    pg_connection = pg_hook.get_conn()
+    cursor = pg_connection.cursor()
+    cursor.execute(stock_tiendas_query)
+    results = cursor.fetchall()
+    results=pd.DataFrame(results)
+    results.columns = ["fecha","ref_id","id_tienda","stock_l8","stock_janis","stock_calculado","multiplicador_medida"]
+    cursor.close()
+    pg_connection.close()
+    return results
 
 def skus_carnes_padre_hijo():
-    host = "bi-ecommerce-postgres-prod-master.cuuchupawrpt.us-east-1.rds.amazonaws.com"
-    database = "postgres"
-    username = "pmardonesg"
-    password = "GOkh_noaql"
-    conn_url = "postgresql+psycopg2://"+username+":"+password+"@"+host+":5432/"+database
-    engine = sqlalchemy.create_engine(conn_url)
-
-    conn = psycopg2.connect(database=database,
-                            host=host,
-                            user=username,
-                            password=password,
-                            port=5432)
-
-    cursor = conn.cursor()
-    cursor.execute("""select sk.erp_id ,sk.ref_id, sk.nombre_sku, st.c1, st.id_tienda
+    import pandas as pd
+    stock_tiendas_query = """select sk.erp_id ,sk.ref_id, sk.nombre_sku, st.c1, st.id_tienda
                     from ecommdata.skus as sk
                     left join ecommdata.stock as st
                     on sk.ref_id = st.ref_id
                     where substring(sk.ref_id,strpos(sk.ref_id,'-')+1,length(sk.ref_id)-strpos(sk.ref_id,'-')) in ('KG','KGV')
                     and split_part(sk.ref_id,'-',1) <> erp_id
-                    and st.c1 = 'Carnes'""")
-    df_base = cursor.fetchall()
-    df_base=pd.DataFrame(df_base)
-    df_base.columns = ["material","ref_id","descripcion","categoria","id_tienda"]
-    #conn.close()
-    
-    return df_base
+                    and st.c1 = 'Carnes'"""
+    pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
+    pg_connection = pg_hook.get_conn()
+    cursor = pg_connection.cursor()
+    cursor.execute(stock_tiendas_query)
+    results = cursor.fetchall()
+    results=pd.DataFrame(results)
+    results.columns = ["material","ref_id","descripcion","categoria","id_tienda"]
+    cursor.close()
+    pg_connection.close()
+    return results
 
 def render_netezza_view(id_tienda,id_material,ds):
-    sql_str= "SELECT sa.SKU_PRODUCT AS material , NBR_ITM AS stock , ou.ou_id AS id_tienda , SA.NM AS nombre , DATE_VALUE as fecha FROM DWC_SMU.SMU.VW_FACT_STOCK S LEFT JOIN DWC_SMU.SMU.VW_DIM_SKU_ATTR SA ON SA.SKU_KEY = S.SKU_KEY LEFT JOIN DWC_SMU.SMU.VW_DIM_ORGANIZATION_UNIT OU ON OU.OU_KEY = S.OU_KEY LEFT JOIN DWC_SMU.SMU.VW_DIM_ALMACEN A ON A.ALMACEN_KEY =S.ALMACEN_KEY LEFT JOIN DWC_SMU.SMU.VW_DIM_PARTICULARIDAD PART ON S.PARTICULARIDAD_KEY =PART.PARTICULARIDAD_KEY WHERE A.ALMACEN_COD = '0001' AND S.APLICA_STOCK = 'S' AND DATE_VALUE = '"+ds+"'::date AND OU.OU_ID in ('{id_t}') AND PART.PARTICULARIDAD_COD = 'A' AND S.TIPO_STOCK_KEY IN (9161419180, 9145314683) AND sa.SKU_PRODUCT in ('{id_m}');"
-
-    dsn_database = "NZ_BU" 
-    dsn_hostname = "10.43.223.12"
-    dsn_port = "5480" 
-    dsn_uid = "pamardonesg"
-    dsn_pwd = "Josofia22."
-    jdbc_driver_name = "org.netezza.Driver" 
-    jdbc_driver_loc = os.path.join('C:/Users/pmardonesg/Desktop/cuadratura/nzjdbc.jar')
-
-    connection_string='jdbc:netezza://'+dsn_hostname+':'+dsn_port+'/'+dsn_database
     
-    conn = jaydebeapi.connect(jdbc_driver_name, 
-                                connection_string, {'user': dsn_uid, 'password': dsn_pwd},
-                                jars=jdbc_driver_loc)
-
-    cur = conn.cursor()
-    cur.execute(sql_str.format(id_t= id_tienda,id_m = id_material))
-    df = cur.fetchall()
-    #print(df)
-    #cur.close()
-    #conn.close()
 
     return df
