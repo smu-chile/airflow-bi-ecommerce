c6474c4ce4e4ce42a4216b184790e7c867f3ddde
Patricio <131201407+PatricioMardonesDev@users.noreply.github.com>
2023-10-10 10:27:38 -0300

    fix_stocks_seguridad_query_promos
    back to last logic

--- dags/etl_stock_seguridad_mfc.py
+++ dags/etl_stock_seguridad_mfc.py
@@ -76,8 +76,8 @@ def promociones(ds):
                             fecha_fin_de_promocion,
                             id_mecanica
                             from ecommdata.workflow_promociones 
-                            where id_mecanica in (26)
-                            and  id_evento in (400)
+                            and wp.id_mecanica not in (25,26,27,36,50,67,72,84,99,37,51,53,59,77,82,93,96)
+                            and wp.id_evento not in (551)
                             and fecha_inicio_de_promocion <= '"""+ds+"""'::date
                             and fecha_fin_de_promocion >= '"""+ds+"""'::date) as _t
                             """
