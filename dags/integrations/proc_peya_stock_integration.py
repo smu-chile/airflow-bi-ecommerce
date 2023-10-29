c6b1d58f07f6ff142db8b4b555a9ec36aaffd4f3
ngonzalezma <ngonzalezma@smu.cl>
2023-10-28 21:12:33 -0300

    Fix espacio fantasma

--- dags/integrations/proc_peya_stock_integration.py
+++ dags/integrations/proc_peya_stock_integration.py
@@ -4,9 +4,9 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.hooks.S3_hook import S3Hook
 from airflow.models import Variable
-​
+
 import pendulum
-​
+
 def _get_peya_active_stores():
     peya_stores_query = """
         SELECT id, id_peya
@@ -21,7 +21,7 @@ def _get_peya_active_stores():
     cursor.close()
     pg_connection.close()
     return results
-​
+
 def _get_peya_botilleria_active_stores():
     peya_stores_query = """
         SELECT id, id_peya_botilleria
@@ -36,7 +36,7 @@ def _get_peya_botilleria_active_stores():
     cursor.close()
     pg_connection.close()
     return results
-​
+
 def _get_peya_market_active_stores():
     peya_stores_query = """
         SELECT id, peya_market
@@ -51,17 +51,15 @@ def _get_peya_market_active_stores():
     cursor.close()
     pg_connection.close()
     return results
-​
+
 def _join_stock_and_promo_prices_from_s3(ds, ti):
     import io
     import pandas as pd
-​
     exec_date = ds.replace("-", "/")
-​
+
     peya_stores = ti.xcom_pull(key="return_value", task_ids=["get_peya_active_stores"])[0]
     peya_store_ids = dict([(peya_store_id[0], peya_store_id[1]) for peya_store_id in peya_stores])
     print(peya_store_ids)
-​
     peya_botilleria_stores = ti.xcom_pull(key="return_value", task_ids=["get_peya_botilleria_active_stores"])[0]
     peya_botilleria_store_ids = dict([(peya_store_id[0], peya_store_id[1]) for peya_store_id in peya_botilleria_stores])
     print(f"Botilleria: {peya_botilleria_store_ids}")
@@ -69,21 +67,19 @@ def _join_stock_and_promo_prices_from_s3(ds, ti):
     peya_market_stores = ti.xcom_pull(key="return_value", task_ids=["get_peya_market_active_stores"])[0]
     peya_market_store_ids = dict([(peya_store_id[0], peya_store_id[1]) for peya_store_id in peya_market_stores])
     print(f"Market: {peya_market_store_ids}")
-​
+
     s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
     s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
-​
     pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
     pg_connection = pg_hook.get_conn()
     cursor = pg_connection.cursor()
-​
+
     for store_id in peya_store_ids.keys():
         print(f"PEYA id: {peya_store_ids[store_id]}")
         join_file_name = f"integraciones/last_millers/stock/out/peya/{exec_date}/{peya_store_ids[store_id]}.csv"
         if s3_hook.check_for_key(join_file_name, bucket_name=s3_bucket):
             print(f"File {join_file_name} already exists on bucket: {s3_bucket}. Skipping...")
             continue
-​
         peya_stock_query = f"""
             SELECT
                 lspp.ean AS ean,
@@ -104,20 +100,18 @@ def _join_stock_and_promo_prices_from_s3(ds, ti):
                     and lspp.id_tienda <> '0755'
                 AND lspp.id_tienda = '{store_id}'
             ;
-​
+
         """
-​
         cursor.execute(peya_stock_query)
         results = cursor.fetchall()
         columns = [i[0] for i in cursor.description]
-​
+
         if len(results) == 0:
             print(f"No records found for Store Id: {store_id}")
             continue
-​
         df = pd.DataFrame(results, columns=columns)
         print(f"Number of records found on stock: {len(df.index)}")
-​
+
         df.columns = map(str.upper, df.columns)
         df["EAN"] = df["EAN"].astype("int64")
         
@@ -126,24 +120,21 @@ def _join_stock_and_promo_prices_from_s3(ds, ti):
         print(f"Checking for previous executions on {prev_join_file_name}.")
         if s3_hook.check_for_key(prev_join_file_name, bucket_name=s3_bucket):
             print(f"Looking for missing products from previous execution on file {prev_join_file_name}.")
-​
             prev_stock_file = s3_hook.get_key(prev_join_file_name, bucket_name=s3_bucket)
             df_prev = pd.read_csv(prev_stock_file.get()["Body"])
-​
+
             df_prev = df_prev[~df_prev["EAN"].isin(df["EAN"])]
             df_prev = df_prev[df_prev["STOCK"]==1]
             df_prev["STOCK"] = 0
-​
+
             print(f"Adding {len(df_prev.index)} missing products as inactive: STOCK = 0.")
-​
             df = pd.concat([df, df_prev])
             
         print(f"Total number of records: {len(df.index)}.")
-​
+
         buffer = io.StringIO()
         df.to_csv(buffer, header=True, index=False, encoding="utf-8")
         buffer.seek(0)
-​
         s3_hook.load_string(buffer.getvalue(),
                     key=join_file_name,
                     bucket_name=s3_bucket,
@@ -178,38 +169,32 @@ def _join_stock_and_promo_prices_from_s3(ds, ti):
     cursor.close()
     pg_connection.close()
     return
-​
+
 def _send_joined_data_to_stfp(ds):
     import os
     import pysftp
-​
     ftp_host = Variable.get("PEYA_SFTP_HOST")
     ftp_port = 22
     ftp_user = Variable.get("PEYA_SFTP_USER")
     ftp_rsa_key = Variable.get("PEYA_SFTP_SECRET_RSA_KEY")
-​
+
     with open("temp_peya_sftp_rsa_key", "w") as key_file:
         key_file.write(ftp_rsa_key)
-​
     exec_date = ds.replace("-", "/")
     prefix = f"integraciones/last_millers/stock/out/peya/{exec_date}/"
-​
+
     s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
     s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
-​
     s3_file_list = s3_hook.list_keys(s3_bucket, prefix=prefix)
-​
+
     print(f"Number of files found: {len(s3_file_list)}")
-​
     for stock_file in s3_file_list:
         print(stock_file)
-​
+
         stock_object = s3_hook.get_key(stock_file, bucket_name=s3_bucket)
         stock_object_body = stock_object.get()["Body"]
-​
         output_stock_file = stock_file.split("/")[-1]
         print(f"File to load to SFTP Server: {output_stock_file}")
-​
         with pysftp.Connection(host=ftp_host, 
                                 username=ftp_user, 
                                 port=ftp_port, 
@@ -219,11 +204,10 @@ def _send_joined_data_to_stfp(ds):
             sftp.putfo(localFile, remotePath)
         
         print("File loaded.")
-​
+
     os.remove("temp_peya_sftp_rsa_key")
-​
     return
-​
+
 default_args = {
     "owner": "ecommerce_ops",
     "depends_on_past": False,
@@ -242,7 +226,7 @@ with DAG(
     concurrency=2,
     tags=["OPS", "last_millers", "dw", "stock", "precios"],
 ) as dag:
-​
+
     dag.doc_md = """
     Cruce de stock, precios y precios promocionales simples para integración con Last Millers: **Pedidos Ya**. \n
     * Se obtiene listado de tiendas activas para la integración PEYA (`registros con id_peya NOT NULL de la tabla integraciones.tiendas_last_millers`). \n
@@ -256,12 +240,11 @@ with DAG(
     * Finalmente, se itera sobre los archivos generados, dejando cada uno de estos en el servidor SFTP de Pedidos Ya.
     Este DAG depende del DAG: [ **proc_stock_last_millers** ].
     """ 
-​
     t0 = PythonOperator(
         task_id = "get_peya_active_stores",
         python_callable = _get_peya_active_stores
     )
-​
+
     t1 = PythonOperator(
         task_id = "get_peya_botilleria_active_stores",
         python_callable = _get_peya_botilleria_active_stores
@@ -270,17 +253,16 @@ with DAG(
         task_id = "get_peya_market_active_stores",
         python_callable = _get_peya_market_active_stores
     )
-​
+
     t3 = PythonOperator(
         task_id = "join_stock_and_promo_prices_from_s3",
         python_callable = _join_stock_and_promo_prices_from_s3
     )
-​
     t4 = PythonOperator(
         task_id = "send_joined_data_to_stfp",
         python_callable = _send_joined_data_to_stfp
     )
-​
+
     t0 >> t3
     t1 >> t3
     t2 >> t3
