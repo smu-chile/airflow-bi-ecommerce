e0a84096bad72a1b78f42c5c563462d5f405c3f9
ngonzalezma <ngonzalezma@smu.cl>
2023-11-23 11:03:04 -0400

    Fix

--- dags/integrations/sql/insert_stock_prices_promos.sql
+++ dags/integrations/sql/insert_stock_prices_promos.sql
@@ -58,7 +58,7 @@ left join (
     and wp.registro_valido = True
     and wp.organizacion_ventas = '1000'
     and wp.canal_distribucion = '10'
-   	and (wp.id_mecanica not in (25, 26, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99) or (wp.n_promocion = 5510012023) or wp.n_promocion = 5680022023)
+   	and (wp.id_mecanica not in (25, 26, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99) or (wp.n_promocion = 5510012023) or wp.n_promocion != 5680022023)
     group by wp.ean
 ) _t3
 on _t.ean = _t3.ean
@@ -125,7 +125,7 @@ left join (
     and wp.registro_valido = True
     and wp.organizacion_ventas = '1000'
     and wp.canal_distribucion = '10'
-	and (wp.id_mecanica not in (25, 26, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99) or (wp.n_promocion = 5510012023) or (wp.n_promocion = 5680022023))
+	and (wp.id_mecanica not in (25, 26, 27, 36, 37, 50, 51, 53, 67, 72, 77, 93, 99) or (wp.n_promocion = 5510012023) or (wp.n_promocion != 5680022023))
     group by wp.ean
 ) _t3
 on _t.ean = _t3.ean
