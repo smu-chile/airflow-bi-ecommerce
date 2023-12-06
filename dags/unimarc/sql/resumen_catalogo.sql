9851212564d29f014a5123fe4725e2350478e72e
mseguraSMU <104867325+mseguraSMU@users.noreply.github.com>
2023-12-06 10:37:57 -0300

    Update resumen_catalogo.sql

--- dags/unimarc/sql/resumen_catalogo.sql
+++ dags/unimarc/sql/resumen_catalogo.sql
@@ -1,7 +1,7 @@
 insert into catalogo.resumen_catalogo
 select fecha_hora::date as fecha, id_tienda, sum(total_surtido) as total_surtido, sum(publicacion_valida) as publicacion_valida, sum(disponible_web) as disponible_web
 from catalogo.publicacion_dia_tienda_surtido pdts
-where fecha_hora::time = '12:00:00' and fecha_hora::date == '{{ds}}'
+where fecha_hora::time = '12:00:00' and fecha_hora::date = '{{ds}}'
 group by fecha_hora, id_tienda
 on conflict (fecha, id_tienda) do update
 set fecha = EXCLUDED.fecha
