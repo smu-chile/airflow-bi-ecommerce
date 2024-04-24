81232e1f8966afe9fb4871bc31489d1df8636694
sgilSMU <138702808+sgilSMU@users.noreply.github.com>
2024-04-24 10:16:41 -0400

    create slack bot for MFC staffing

--- dags/unimarc/etl_turno_supervisor_slack.py
+++ dags/unimarc/etl_turno_supervisor_slack.py
@@ -6,71 +6,89 @@ from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 
-from pulp import LpProblem, LpVariable, lpSum, LpMinimize, LpMaximize
 from datetime import datetime, timedelta
 import pendulum
 
-def create_schedule(data):
+def create_schedule(operadores_df, tareas_df):
     import pandas as pd
-    # Create a new LP problem
-    prob = LpProblem("Scheduler", LpMinimize)
+    column_names = [f"{hour:02d}:00" for hour in range(7, 24)] + [f"{hour:02d}:00" for hour in range(0, 7)]
+    schedule_df = pd.DataFrame(index=operadores_df['rut'], columns=column_names)
+    schedule_df = schedule_df.fillna(0)
+
+    def is_within_task_time(hour, start_time, end_time):
+        start_hour = start_time.hour
+        end_hour = end_time.hour
+        if end_hour < start_hour:  
+            return (start_hour <= hour < 24) or (0 <= hour < end_hour)
+        else:  
+            return start_hour <= hour < end_hour
+
+    for hour in range(24):
+        for _, task in tareas_df.iterrows():
+            task_id = task['id_tarea']
+            task_start_time = task['hora_inicio']
+            task_end_time = task['hora_termino']
+            min_workers = task['min_operadores']
+            max_workers = task['max_operadores']
+            duration = task['duracion']
+
+            if duration > 0 and is_within_task_time(hour, task_start_time, task_end_time):
+                workers_needed = min(min_workers, max_workers - schedule_df.iloc[:, hour].eq(task_id).sum())
+                for _, worker in operadores_df.iterrows():
+                    worker_name = worker['rut']
+                    entrada = int(worker['entrada'].split(':')[0])
+                    salida = int(worker['salida'].split(':')[0])
+
+                    if workers_needed > 0:
+                        if salida < entrada:
+                            if (entrada <= hour < 24) or (0 <= hour < salida):
+                                if schedule_df.loc[worker_name, f"{hour:02d}:00"] == 0:
+                                    schedule_df.loc[worker_name, f"{hour:02d}:00"] = task_id
+                                    workers_needed -= 1
+                                    duration -= 1
+                        else:
+                            if entrada <= hour < salida:
+                                if schedule_df.loc[worker_name, f"{hour:02d}:00"] == 0:
+                                    schedule_df.loc[worker_name, f"{hour:02d}:00"] = task_id
+                                    workers_needed -= 1
+                                    duration -= 1
+
+    def is_within_shift(hour, entrada, salida):
+        if salida < entrada:
+            return (entrada <= hour < 24) or (0 <= hour < salida)
+        else:
+            return entrada <= hour < salida
     
-    # Extract data
-    tasks = data['id_tarea'].tolist()
-    start_times = data['hora_inicio'].tolist()
-    end_times = data['hora_termino'].tolist()
-    durations = data['duracion'].tolist()
-    priorities = data['prioridad'].tolist()
-    min_workers = data['min_operadores'].tolist()
-    max_workers = data['max_operadores'].tolist()
+    for hour in range(24):
+        for _, task in tareas_df.iterrows():
+            task_id = task['id_tarea']
+            task_start_time = task['hora_inicio']
+            task_end_time = task['hora_termino']
+            min_workers = task['min_operadores']
+            duration = task['duracion']
 
-    # Define the decision variables
-    # Each variable represents whether a task is scheduled at each hour for each worker
-    schedule = {(task, hour, worker): LpVariable(f'Task_{task}_Hour_{hour}_Worker_{worker}', cat='Binary')
-                for task in tasks
-                for hour in range(24)
-                for worker in range(max_workers[task])}
+            if duration > 0 and is_within_task_time(hour, task_start_time, task_end_time):
+                workers_assigned = schedule_df[f"{hour:02d}:00"].eq(task_id).sum()
+                remaining_workers = min_workers - workers_assigned
 
-    # Define the objective function
-    # Minimize the sum of priorities if not enough workers are available
-    prob += lpSum(priorities[task] * lpSum(schedule[task, hour, worker]
-                                            for hour in range(24)
-                                            for worker in range(max_workers[task]))
-                  for task in tasks)
+                if remaining_workers > 0:
+                    for _, worker in operadores_df.iterrows():
+                        worker_name = worker['rut']
+                        entrada = int(worker['entrada'].split(':')[0])
+                        salida = int(worker['salida'].split(':')[0])
 
-    # Constraints
-    for task in tasks:
-        # Ensure each task is scheduled for its duration
-        prob += lpSum(schedule[task, hour, worker] for hour in range(24) for worker in range(max_workers[task])) == durations[task]
+                        if is_within_shift(hour, entrada, salida):
+                            if schedule_df.loc[worker_name, f"{hour:02d}:00"] == 0:
+                                schedule_df.loc[worker_name, f"{hour:02d}:00"] = task_id
+                                remaining_workers -= 1
+                                duration -= 1
 
-        # Ensure tasks only happen within their allowed time range
-        for hour in range(24):
-            for worker in range(max_workers[task]):
-                if hour < start_times[task] or hour + durations[task] > end_times[task]:
-                    prob += schedule[task, hour, worker] == 0
-
-        # Ensure minimum and maximum number of workers for each task
-        prob += lpSum(schedule[task, hour, worker]
-                      for hour in range(24)
-                      for worker in range(max_workers[task])) >= min_workers[task]
-        prob += lpSum(schedule[task, hour, worker]
-                      for hour in range(24)
-                      for worker in range(max_workers[task])) <= max_workers[task]
-
-    # Solve the problem
-    prob.solve()
-
-    # Extract the schedule
-    schedule_df = pd.DataFrame(columns=['Task', 'Hour', 'Worker'])
-    for task in tasks:
-        for hour in range(24):
-            for worker in range(max_workers[task]):
-                if schedule[task, hour, worker].varValue == 1:
-                    schedule_df = schedule_df.append({'Task': task, 'Hour': hour, 'Worker': worker}, ignore_index=True)
+                                if remaining_workers == 0 or duration == 0:
+                                    break
 
     return schedule_df
 
-def turnos_load_to_s3(ds):
+def tareas_load_to_s3(ds):
     import pandas as pd
     import numpy as np
     import os
@@ -86,22 +104,23 @@ def turnos_load_to_s3(ds):
     curr_working_directory = os.getcwd()
     print(os.getcwd())
 
-    with open(f"{curr_working_directory}/dags/unimarc/sql/turnos_mfc.sql", "r") as query_file:
-        promociones_query = query_file.read()
-    
-    promociones_query = promociones_query.replace("{ds}", ds)
+    with open(f"{curr_working_directory}/dags/unimarc/sql/dotacion_tareas.sql", "r") as query_file:
+        tareas_query = query_file.read()
+
+    tareas_query = tareas_query.replace("{ds}", ds)
 
     print("Base query:")
-    print(promociones_query)
+    print(tareas_query)
 
     pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
     pg_connection = pg_hook.get_conn()
 
-    df_promotions = pd.read_sql_query(promociones_query, pg_connection)
-    buffer = io.StringIO()
-    df_promotions.to_csv(buffer, header=True, index=False, encoding="utf-8")
+    df_tareas = pd.read_sql_query(tareas_query, pg_connection)
 
-    filename = f"promociones_vtex_alvi/{exec_date}/turnos_supervisor.csv"
+    buffer = io.StringIO()
+    df_tareas.to_csv(buffer, header=True, index=False, encoding="utf-8")
+
+    filename = f"dotacion/{exec_date}/tareas_MFC.csv"
 
     buffer.seek(0)
     print("se logro transformar el dataframe a un archivo .csv")
@@ -116,102 +135,196 @@ def turnos_load_to_s3(ds):
 
     return filename
 
-def turnos_load_to_postgres(ti):
-    import numpy as np
+def operadores_load_to_s3(ds):
     import pandas as pd
-    import sqlalchemy
-    from sqlalchemy import text
+    import numpy as np
+    import os
+    import io
+    from io import StringIO
 
-    filename = ti.xcom_pull(key="return_value", task_ids=["turnos_load_to_s3"])[0]
-
+    exec_date = ds.replace("-", "/")
+    prefix = f"dotacion/{exec_date}/"
     s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
+
     s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
 
-    print("Searching file: "+filename)
-    if not s3_hook.check_for_key(filename, bucket_name=s3_bucket):
-        raise Exception("Key %s does not exist." % filename)
+    curr_working_directory = os.getcwd()
+    print(os.getcwd())
 
-    turnos_object = s3_hook.get_key(filename, bucket_name=s3_bucket)
+    with open(f"{curr_working_directory}/dags/unimarc/sql/dotacion_operadores.sql", "r") as query_file:
+        operadores_query = query_file.read()
 
-    df = pd.read_csv(turnos_object.get()["Body"])
-    if len(df.index) == 0:
-        print("There are no new nor updated records to load. Task will exit as successfull.")
-        return
+    operadores_query = operadores_query.replace("{ds}", ds)
+
+    print("Base query:")
+    print(operadores_query)
+
+    pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
+    pg_connection = pg_hook.get_conn()
+
+    df_operadores = pd.read_sql_query(operadores_query, pg_connection)
+
+    buffer = io.StringIO()
+    df_operadores.to_csv(buffer, header=True, index=False, encoding="utf-8")
+
+    filename = f"dotacion/{exec_date}/operadores_MFC.csv"
+
+    buffer.seek(0)
+    print("se logro transformar el dataframe a un archivo .csv")
+    print(f"con fecha {ds} y nombre de filename como {filename}")
+    s3_hook.load_string(buffer.getvalue(),
+                key=filename,
+                bucket_name=s3_bucket,
+                replace=True,
+                encrypt=False)
     
-    print(f"Number of records extracted: {len(df.index)}")
-    df.info()
+    print(f"File load on S3: {prefix}")
 
-    schedule = create_schedule(df)
-    print(schedule)
+    return filename
 
-    host = Variable.get("POSTGRESQL_HOST")
-    database = Variable.get("POSTGRESQL_DB")
-    username = Variable.get("POSTGRESQL_USER")
-    password = Variable.get("POSTGRESQL_PASSWORD")
-    
-    conn_url = f"postgresql+psycopg2://{username}:{password}@{host}:5432/{database}"
-    engine = sqlalchemy.create_engine(conn_url)
-
-    # Save to PostgreSQL:
-
-    with engine.begin() as conn:
-        conn.execute("TRUNCATE ecommdata.turnos_supervisor_mfc") 
-        df.to_sql(name="turnos_supervisor_mfc",
-                    con=conn,         
-                    schema="ecommdata",         
-                    if_exists='append',         
-                    index=False,         
-                    chunksize=20000,         
-                    method='multi')
-
-    print("Data loaded to Postgres: ecommdata.turnos_supervisor_mfc")
-    return
-
-def turnos_load_to_slack(ti):
+def disponibilidad_load_to_s3(ds):
     import pandas as pd
+    import numpy as np
+    import os
+    import io
+    from io import StringIO
+
+    exec_date = ds.replace("-", "/")
+    prefix = f"dotacion/{exec_date}/"
+    s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
+
+    s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
+
+    curr_working_directory = os.getcwd()
+    print(os.getcwd())
+
+    with open(f"{curr_working_directory}/dags/unimarc/sql/dotacion_disponible.sql", "r") as query_file:
+        disponibilidad_query = query_file.read()
+
+    disponibilidad_query = disponibilidad_query.replace("{ds}", ds)
+
+    print("Base query:")
+    print(disponibilidad_query)
+
+    pg_hook = PostgresHook(postgres_conn_id="postgresql_conn")
+    pg_connection = pg_hook.get_conn()
+
+    df_disponibilidad = pd.read_sql_query(disponibilidad_query, pg_connection)
+
+    buffer = io.StringIO()
+    df_disponibilidad.to_csv(buffer, header=True, index=False, encoding="utf-8")
+
+    filename = f"dotacion/{exec_date}/disponibilidad_MFC.csv"
+
+    buffer.seek(0)
+    print("se logro transformar el dataframe a un archivo .csv")
+    print(f"con fecha {ds} y nombre de filename como {filename}")
+    s3_hook.load_string(buffer.getvalue(),
+                key=filename,
+                bucket_name=s3_bucket,
+                replace=True,
+                encrypt=False)
+    
+    print(f"File load on S3: {prefix}")
+
+    return filename
+
+def turnos_load_to_slack(ti,ds):
+    import pandas as pd
+    import io
     from slack_sdk import WebClient
     from slack_sdk.errors import SlackApiError
 
-    filename = ti.xcom_pull(key="return_value", task_ids=["turnos_load_to_s3"])[0]
+    tareas_filename = ti.xcom_pull(key="return_value", id_tareas=["tareas_load_to_s3"])[0]
+    operadores_filename = ti.xcom_pull(key="return_value", id_tareas=["operadores_load_to_s3"])[0]
+    disponibilidad_filename = ti.xcom_pull(key="return_value", id_tareas=["disponibilidad_load_to_s3"])[0]
 
     s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
     s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
 
-    print("Searching file: "+filename)
-    if not s3_hook.check_for_key(filename, bucket_name=s3_bucket):
-        raise Exception("Key %s does not exist." % filename)
+    print("Searching file: "+tareas_filename)
+    if not s3_hook.check_for_key(tareas_filename, bucket_name=s3_bucket):
+        raise Exception("Key %s does not exist." % tareas_filename)
 
-    turnos_object = s3_hook.get_key(filename, bucket_name=s3_bucket)
+    tareas_object = s3_hook.get_key(tareas_filename, bucket_name=s3_bucket)
 
-    df = pd.read_csv(turnos_object.get()["Body"])
-    if len(df.index) == 0:
+    print("Searching file: "+operadores_filename)
+    if not s3_hook.check_for_key(operadores_filename, bucket_name=s3_bucket):
+        raise Exception("Key %s does not exist." % operadores_filename)
+
+    operadores_object = s3_hook.get_key(operadores_filename, bucket_name=s3_bucket)
+
+    print("Searching file: "+disponibilidad_filename)
+    if not s3_hook.check_for_key(disponibilidad_filename, bucket_name=s3_bucket):
+        raise Exception("Key %s does not exist." % disponibilidad_filename)
+
+    disponibilidad_object = s3_hook.get_key(disponibilidad_filename, bucket_name=s3_bucket)
+
+    df_tareas = pd.read_csv(tareas_object.get()["Body"])
+    if len(df_tareas.index) == 0:
         print("There are no new nor updated records to load. Task will exit as successfull.")
         return
-    headers = ["fecha", "bloque", "numero_operadores", "entrada", "salida"]
+    
+    df_operadores = pd.read_csv(operadores_object.get()["Body"])
+    if len(df_tareas.index) == 0:
+        print("There are no new nor updated records to load. Task will exit as successfull.")
+        return
+    
+    df_disponibilidad = pd.read_csv(disponibilidad_object.get()["Body"])
+    if len(df_tareas.index) == 0:
+        print("There are no new nor updated records to load. Task will exit as successfull.")
+        return
+    
+    schedule_df = create_schedule(df_operadores,df_tareas)
 
-    column_widths = [len(header) for header in headers]
-    for row in df:
-        for index, value in enumerate(row):
-            column_widths[index] = max(column_widths[index], len(str(value)))
-
-    formatted_header = " | ".join(header.upper().ljust(column_widths[index]) for index, header in enumerate(headers))
-
-    formatted_rows = [formatted_header]
-    for row in df:
-        formatted_row = " | ".join(str(value).ljust(column_widths[index]) for index, value in enumerate(row))
-        formatted_rows.append(formatted_row)
-
-    formatted_message = "```\n" + "\n".join(formatted_rows) + "\n```"
-    print(formatted_message)
-
+    task_id_to_task = dict(zip(df_tareas['id_tarea'], df_tareas['nombre_tarea']))
+    rut_to_nombre_operador = dict(zip(df_operadores['rut'], df_operadores['nombre_operador']))
+    schedule_df = schedule_df.apply(lambda x: x.map(task_id_to_task) if x.name != 'rut' else x)
+    schedule_df.index = schedule_df.index.map(rut_to_nombre_operador)
+    schedule_df.index.name = 'nombre_operador'
+    
     token = Variable.get("token_slack")
-
     client = WebClient(token=token)
 
-    try:
-        response = client.chat_postMessage(channel="dotaciones-mfc", text=formatted_message)
-    except SlackApiError as e:
-        print(f"Error sending message: {e}")
+    with io.BytesIO() as buffer:
+        df_tareas.to_csv(buffer, index=False, encoding='utf-8')
+        buffer.seek(0)
+        try:
+            response = client.files_upload(
+                channels="cambio-de-turno-supervisores-mfc",
+                file=buffer,
+                filename=f"tareas_mfc_{ds}.csv",
+                title="Tareas Diarias",
+                initial_comment="Se adjunta el reporte de tareas para el dia."
+            )
+        except SlackApiError as e:
+            print(f"Error al subir archivo: {e}")
+    with io.BytesIO() as buffer:
+        df_disponibilidad.to_csv(buffer, index=False, encoding='utf-8')
+        buffer.seek(0)
+        try:
+            response = client.files_upload(
+                channels="cambio-de-turno-supervisores-mfc",
+                file=buffer,
+                filename=f"dotacion_diaria_{ds}.csv",
+                title="Distribucion de operadores por turnos",
+                initial_comment="Se adjunta el reporte de distribucion de operadores por turnos para el dia."
+            )
+        except SlackApiError as e:
+            print(f"Error al subir archivo: {e}")
+    with io.BytesIO() as buffer:
+        schedule_df.to_csv(buffer, index=True, encoding='utf-8')
+        buffer.seek(0)
+        try:
+            response = client.files_upload(
+                channels="cambio-de-turno-supervisores-mfc",
+                file=buffer,
+                filename=f"tareas_por_operador_mfc_{ds}.csv",
+                title="Distribucion de turnos MFC",
+                initial_comment="Se adjunta el reporte de tareas por operador para el dia."
+            )
+        except SlackApiError as e:
+            print(f"Error al subir archivo: {e}")
 
     return
     
@@ -227,7 +340,7 @@ with DAG(
     'etl_turno_supervisor_mfc',
     default_args=default_args,
     description="consulta de datos de Stock MFC, maestra reposicion desde postgres para logica de reposicion.",
-    schedule_interval="0 8 * * *",
+    schedule_interval="0 6 * * 1,2,3,4,5",
     start_date=pendulum.datetime(2022, 8, 25, tz="America/Santiago"),
     catchup=False,
     max_active_runs = 1,
@@ -239,12 +352,20 @@ with DAG(
     """ 
 
     t0 = PythonOperator(
-        task_id = "turnos_load_to_s3",
-        python_callable = turnos_load_to_s3
+        id_tarea = "tareas_load_to_s3",
+        python_callable = tareas_load_to_s3
     )
     t1 = PythonOperator(
-        task_id = "turnos_load_to_postgres",
-        python_callable = turnos_load_to_postgres
+        id_tarea = "operadores_load_to_s3",
+        python_callable = operadores_load_to_s3
+    )
+    t2 = PythonOperator(
+        id_tarea = "disponibilidad_load_to_s3",
+        python_callable = disponibilidad_load_to_s3
+    )
+    t3 = PythonOperator(
+        id_tarea = "turnos_load_to_slack",
+        python_callable = turnos_load_to_slack
     )
 
-    t0 >> t1
+    t0 >> t1 >> t2 >> t3
--- dags/unimarc/sql/turnos_mfc.sql
+++
@@ -1,8 +0,0 @@
-select dm.fecha,
-	dm.bloque,
-	count(dm.rut) as numero_operadores,
-	dm.entrada,
-	dm.salida
-from ecommdata.dotacion_mfc dm
-where dm.fecha = '{ds}'::date +1
-group by dm.bloque,dm.fecha,dm.entrada,dm.salida
