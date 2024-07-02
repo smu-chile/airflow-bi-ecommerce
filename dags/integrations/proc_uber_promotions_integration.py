8177ff6061d0491b2b2b58b6a9854d984604287b
ngonzalezma <ngonzalezma@smu.cl>
2024-07-02 08:49:04 -0400

    Correccion lastmiller

--- dags/integrations/proc_uber_promotions_integration.py
+++ dags/integrations/proc_uber_promotions_integration.py
@@ -335,7 +335,7 @@ def _join_promo_prices_test_from_s3(ds, ti):
                 AND wp.nombre_promocion::text !~~ '%ESTADO%'::text
                 and wp.nombre_promocion::text !~~ '% LOC%'::text
                 and wp.nombre_promocion::text !~~ '%LIQ%'::text
-                and wp.n_promocion in  ()'5552392024','1120012024',
+                and wp.n_promocion not in  ('5552392024','1120012024',
 '1120022024',
 '1120032024',
 '1120042024',
