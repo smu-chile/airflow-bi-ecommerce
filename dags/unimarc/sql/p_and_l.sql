190446e0c8dd568144b845f0e192e7b925ac5a98
Patricio <131201407+PatricioMardonesDev@users.noreply.github.com>
2024-08-13 15:59:43 -0400

    fix_p_and_l
    fix

--- dags/unimarc/sql/p_and_l.sql
+++ dags/unimarc/sql/p_and_l.sql
@@ -10,7 +10,7 @@ cvst.id_tienda, cvst.nombre_local, msp.categoria_sap,msp.grupo_sap , msp.linea_s
 c.n1 as categoria_ecom_1,c.n2 as categoria_ecom_2, c.n3 as categoria_ecom_3,
 cvst.venta ,cvst.venta_umb ,cvst.costo_neto ,cvst.numero_aprox_trx
 from ecommdata.costos_ventas_sku_tienda cvst 
-left join ecommdata.maestra_sku_proveedor msp on cvst.sku_key = msp.sku_key::varchar
+left join ecommdata.maestra_sku_proveedor msp on cvst.sku_key::float = msp.sku_key::float
 left join ecommdata.productos p on p.ref_id = concat(msp.material, '-',REPLACE(msp.umb, 'ST', 'UN'))
 left join ecommdata.categorias c on c.id = p.id_categoria 
 where id_tienda not in (select id from ecommdata_alvi.tiendas t) --saca Alvi
