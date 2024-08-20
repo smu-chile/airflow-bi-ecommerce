ebd8470cfd314287f91e54e9ddb245b80491f950
Patricio <131201407+PatricioMardonesDev@users.noreply.github.com>
2024-08-20 14:57:35 -0400

    Update etl_carga_tiendas_vtex.py
    update

--- dags/unimarc/etl_carga_tiendas_vtex.py
+++ dags/unimarc/etl_carga_tiendas_vtex.py
@@ -24,22 +24,29 @@ def query_to_df(query):
     pg_connection.close()
     return results
 
-def get_api_data(url, headers=None):
-    import requests
+def get(url, responses, session, exception_cases, X_VTEX_API_AppKey, X_VTEX_API_AppToken):
+    r = session.get(url, headers = {"X-VTEX-API-AppKey" : X_VTEX_API_AppKey, "X-VTEX-API-AppToken" : X_VTEX_API_AppToken})
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Lanza una excepción para códigos de error HTTP
-        return response.json()  # Retorna los datos en formato JSON
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")  # Muestra el error HTTP
-    except Exception as err:
-        print(f"Other error occurred: {err}")  # Muestra cualquier otro error
-    return None
+        responses.append({'json':r.json(), 'url':url})
+    except Exception as e:
+        print(e)
+        print(url)
+        print(r)
+        print(r.status_code)
+        exception_cases.append(url)
+
+def bulk_get(url_sublist, responses, session, exception_cases, X_VTEX_API_AppKey, X_VTEX_API_AppToken):
+    for url in url_sublist:
+        get(url, responses, session, exception_cases, X_VTEX_API_AppKey, X_VTEX_API_AppToken)
+    return
 
 def carga_tiendas_to_s3(ds):
     import pandas as pd
     import io
+    from threading import Thread
     from io import StringIO
+    import requests
+
     exec_date = ds.replace("-", "/")
     date_aux = ds.replace("-", "_")
     prefix = f"carga_tiendas_vtex/{exec_date}/"
@@ -57,7 +64,7 @@ def carga_tiendas_to_s3(ds):
                     and p.ref_id is not null
                     and s.ref_id is not null
                     and pe.material is null
-                    limit 100000
+                    limit 1000
                     --union
                     --select pc.ref_id, pc.id_tienda, t.canal_venta_vtex , p.vtex_id 
                     --from ecommdata.publicacion_catalogo pc
@@ -73,28 +80,60 @@ def carga_tiendas_to_s3(ds):
     lista_ref_ids = df['vtex_id'].unique()
     print(f"cantidad de skus unicos: {len(lista_ref_ids)}")
 
-    X_VTEX_API_AppKey = Variable.get("X_VTEX_API_AppKey")
-    X_VTEX_API_AppToken = Variable.get("X_VTEX_API_AppToken")
     account_name = Variable.get("VTEX_ACCOUNT_NAME") 
     env = Variable.get("VTEX_ENV")
 
-    lista_api = []
+    
+    url_list = []
+    for sku in lista_ref_ids:
+        url = f"https://{account_name}.{env}.com.br/api/catalog/pvt/product/{str(int(sku))}/salespolicy"
+        url_list.append(url)
 
-    for aux in lista_ref_ids:
-
-        header = {"X-VTEX-API-AppKey" : X_VTEX_API_AppKey, "X-VTEX-API-AppToken" : X_VTEX_API_AppToken}
-        url = f"https://{account_name}.{env}.com.br/api/catalog/pvt/product/{str(int(aux))}/salespolicy"
-        data = get_api_data(url, header)
-
-        print(data)
-        
-        if data:
-            lista_api.extend(data)
+    session = requests.session()
+    thread_num = 2#40
+    task_num = len(url_list)//thread_num # division entera
+    adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=thread_num)
+    session.mount('https://', adapter)
+    thread_tasks = []
+    count = 0
+    responses = []
+    exception_cases = []
    
-    df_api = pd.DataFrame(lista_api)
+    X_VTEX_API_AppKey = Variable.get("X_VTEX_API_AppKey")
+    X_VTEX_API_AppToken = Variable.get("X_VTEX_API_AppToken")
+    
+    for thr in range(thread_num):
+        new_task = Thread(target=bulk_get, args=[url_list[task_num*count:task_num*(count+1)], responses, session, exception_cases, X_VTEX_API_AppKey, X_VTEX_API_AppToken], daemon=True)
+        new_task.start()
+        thread_tasks.append(new_task)
+        count = count + 1
+    # tareas resagadas:
+    if task_num*thread_num != len(url_list):
+        new_task = new_task = Thread(target=bulk_get, args=[url_list[task_num*thread_num:], responses, session, exception_cases, X_VTEX_API_AppKey, X_VTEX_API_AppToken], daemon=True)
+        new_task.start()
+        thread_tasks.append(new_task)
+    for task in thread_tasks:
+        task.join()
+        thread_tasks = []
+    print(responses)
+    
+    final_responses = []
+
+    for response in responses:
+            response_aux = response['json']
+            try:
+                aux = (response_aux['ProductId'],response_aux['StoreId'])
+                final_responses.append(aux)
+            except KeyError as e:
+                    print(e)
+                    print(response)
+                    exception_cases.append(response['url'])
+    
+    df_tiendas_productos = pd.DataFrame(final_responses)
+    print(df_tiendas_productos.head(30))
         
     buffer = io.StringIO()
-    df_api.to_csv(buffer, header=True, index=False, encoding="utf-8")
+    df_tiendas_productos.to_csv(buffer, header=True, index=False, encoding="utf-8")
     buffer.seek(0)
 
     filename = f"carga_tiendas_vtex/{exec_date}/carga_tiendas_{date_aux}.csv"
