9848efa0d5abfe3f5f1e5665854321e7f3f0246e
Patricio <131201407+PatricioMardonesDev@users.noreply.github.com>
2024-09-02 17:13:12 -0400

    Update etl_ok_to_shop_v2.py
    Qmit

--- dags/etl_ok_to_shop_v2.py
+++ dags/etl_ok_to_shop_v2.py
@@ -8,21 +8,85 @@ from airflow.hooks.S3_hook import S3Hook
 from datetime import datetime
 import pendulum
 
-def get_nutritional_data(url):
+def get_nutritional_data(url, exception_cases, retries=3, backoff_factor=0.3):
     import requests
-    headers = {
-    'version': '1.0.0',
-    'source': 'web',
-    'Connection': 'keep-alive'
-    }
-    #print(f"\n {url}")
-    response = requests.get(url, headers=headers)
+    import time
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        #print(f"Error: {response.status_code}")
-        return None
+    headers = {
+        'version': '1.0.0',
+        'source': 'web',
+        'Connection': 'keep-alive'
+    }
+
+    ean = url.split('/')[-1]
+
+    for attempt in range(retries):
+        try:
+            response = requests.get(url, headers=headers, timeout=10)  # Timeout para evitar colgaduras
+            
+            if response.status_code == 200:
+                print(f"Encontramos data de para este EAN: {ean}")
+                data = response.json()
+                
+                # Extracción de datos básicos
+                ingredients = ", ".join([ingredient["name"] for ingredient in data.get("ingredients", [])])
+                has_alcohol = data.get("hasAlcohol", False)
+
+                # Extracción de datos de la tabla nutricional
+                nutritional_table = data.get("nutritionalTable", {})
+                portion_text = nutritional_table.get("portionText", "")
+                portion_value = nutritional_table.get("portionValue", "")
+                total_portions = nutritional_table.get("totalPortions", "")
+                table_data = nutritional_table.get("table", [])
+
+                # Crear un diccionario con los valores de los nutrientes
+                nutrient_dict = {}
+                if table_data:
+                    for row in table_data[1:]:  # Saltar la primera fila que es el encabezado
+                        nutrient_key = row[0].lower().replace(" ", "_")  # Ej. "Energía (kCal)" -> "energía_(kcal)"
+                        nutrient_dict[f"{nutrient_key}_valor"] = row[1]
+                        nutrient_dict[f"{nutrient_key}_peso"] = row[2] if len(row) > 2 else None
+                        nutrient_dict[f"{nutrient_key}_cant"] = row[2] if len(row) > 2 else None
+
+                # Extracción de suitabilities
+                suitabilities_dict = {}
+                for suit in data.get("suitabilities", []):
+                    suitabilities_dict[suit["code"]] = suit["description"]
+
+                # Extracción de stamps
+                stamps_dict = {}
+                for stamp in data.get("stamps", []):
+                    stamps_dict[stamp["code"]] = stamp["description"]
+
+                # Consolidar todos los datos en un solo diccionario
+                return {
+                    "ean": ean,
+                    "ingredients": ingredients,
+                    "has_alcohol": has_alcohol,
+                    "portion_text": portion_text,
+                    "portion_value": portion_value,
+                    "total_portions": total_portions,
+                    **nutrient_dict,
+                    **suitabilities_dict,
+                    **stamps_dict
+                }
+            else:
+                print(f"Error: Received status code {response.status_code} for URL: {url}")
+                exception_cases.append(url)
+                return None
+            
+        except requests.exceptions.Timeout:
+            print(f"Timeout occurred for URL: {url}")
+            exception_cases.append(url)
+        except requests.exceptions.RequestException as e:
+            print(f"Request exception occurred for URL: {url}: {e}")
+            exception_cases.append(url)
+        
+        # Exponential backoff for retries
+        time.sleep(backoff_factor * (2 ** attempt))
+    
+    print(f"Failed to retrieve data after {retries} attempts for URL: {url}")
+    return None
 
 def query_to_df(query):
     import pandas as pd
@@ -38,46 +102,76 @@ def query_to_df(query):
     pg_connection.close()
     return results
 
+def bulk_get(url_sublist, responses, exception_cases):
+    for url in url_sublist:
+        response = get_nutritional_data(url,exception_cases)  # Solo se pasa el URL a la función
+        if response:
+            responses.append(response)
+        else:
+            exception_cases.append(url)
+    return
+
 def ok_to_shop_api_to_s3(ds):
-    import pandas as pd
     import requests
-    import math
+    from threading import Thread
+    import pandas as pd
     import io
 
     exec_date = ds.replace("-", "/")
     date_aux = ds.replace("-", "_")
-    prefix = f"stock_seguridad_apo/{exec_date}/"
+    prefix = f"ok_to_shop_v2/{exec_date}/"
     s3_bucket = Variable.get("AWS_S3_BUCKET_NAME")
 
     s3_hook = S3Hook(aws_conn_id="aws_s3_connection")
 
-    query_eans = """select distinct ean
-            from ecommdata.sku_ean se 
-            where ref_id in (select concat(material,'-',umv)as ref_id from ecommdata.lista8 l)
+    query_eans = """select distinct se.ean::int8
+        from ecommdata.lista8 l 
+        left join ecommdata.sku_ean se on se.ref_id = concat(l.material,'-',l.umv)
+        where l.material is not null
             """
     
     df = query_to_df(query_eans)
-    df = df.head(50)
-    data = []
+    df = df.dropna(subset=['ean'])
+    lista_eans = df['ean'].unique()
 
-    for index, value in df['ean'].iteritems():
-        url = f"https://bff-unimarc-ecommerce.unimarc.cl/catalog/product/nutritional-data/{str(value)}"
-        json_data = get_nutritional_data(url)
-        if json_data:
-            product_data = {
-                'EAN': value,
-                'ingredients': [ingredient['name'] for ingredient in json_data.get('ingredients', [])],
-                **json_data
-            }
-            data.append(product_data)
+    url_list = [
+        f"https://bff-unimarc-ecommerce.unimarc.cl//catalog/product/nutritional-data/{str(int(ean))}"
+        for ean in lista_eans
+        ]
 
-    df = pd.DataFrame(data)
+    session = requests.session()
+    thread_num = 4#40
+    task_num = len(url_list)//thread_num # division entera
+    adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=thread_num)
+    session.mount('https://', adapter)
+    thread_tasks = []
+    count = 0
+    responses = []
+    exception_cases = []
 
-    print(df)
-    df.info()
+    for thr in range(thread_num):
+        new_task = Thread(target=bulk_get, args=[url_list[task_num*count:task_num*(count+1)], responses, exception_cases], daemon=True)
+        new_task.start()
+        thread_tasks.append(new_task)
+        count = count + 1
+    # tareas resagadas:
+    if task_num*thread_num != len(url_list):
+        new_task = new_task = Thread(target=bulk_get, args=[url_list[task_num*thread_num:], responses, exception_cases], daemon=True)
+        new_task.start()
+        thread_tasks.append(new_task)
+    for task in thread_tasks:
+        task.join()
+        thread_tasks = []
+
+    df_final = pd.DataFrame(responses)
+
+    print(f"\nCantidad de eans que no se encuentran en ok to shop: {len(exception_cases)}")
+
+    print(df_final)
+    df_final.info()
 
     buffer = io.StringIO()
-    df.to_csv(buffer, header=True, index=False, encoding="utf-8")
+    df_final.to_csv(buffer, header=True, index=False, encoding="utf-8")
     filename = f"ok_to_shop_v2/{exec_date}/ok_to_shop_{date_aux}.csv"
     buffer.seek(0)
     print("se logro transformar el dataframe a un archivo .csv")
@@ -112,6 +206,7 @@ with DAG(
 
     dag.doc_md = """
     Cargar datos de eans de productos al consumir API ok_to_shop a postgres y S3
+    upsert
     """
 
     t0 = PythonOperator(
