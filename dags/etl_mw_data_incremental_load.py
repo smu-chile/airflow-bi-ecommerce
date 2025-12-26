ecb56b328d00b7a72448fb265175dc518c11244d
fvargasl <fvargasl@smu.cl>
2025-12-26 11:46:13 -0300

    update mw + sellout
    mw now reprocess last 24hr;
    sell_out now is an upsert (cm)

--- dags/etl_mw_data_incremental_load.py
+++ dags/etl_mw_data_incremental_load.py
@@ -72,8 +72,9 @@ def _payments_incremental_load(ts):
 
     execution_date = datetime.strptime(ts[:10], "%Y-%m-%d")
     local_tz = pytz.timezone("America/Santiago")
-    date_from = local_tz.localize(execution_date).astimezone(pytz.utc)
-    date_to = date_from + timedelta(days=1)
+    date_init = local_tz.localize(execution_date).astimezone(pytz.utc)
+    date_from = date_init - timedelta(hours=24)
+    date_to = date_init
 
     print(date_from)
     print(date_to)
@@ -170,8 +171,9 @@ def _operations_incremental_load(ts, ti):
 
     execution_date = datetime.strptime(ts[:10], "%Y-%m-%d")
     local_tz = pytz.timezone("America/Santiago")
-    date_from = local_tz.localize(execution_date).astimezone(pytz.utc)
-    date_to = date_from + timedelta(days=1)
+    date_init = local_tz.localize(execution_date).astimezone(pytz.utc)
+    date_from = date_init - timedelta(hours=24)
+    date_to = date_init
 
     print(date_from)
     print(date_to)
@@ -475,8 +477,9 @@ def _inscriptions_incremental_load(ts):
 
     execution_date = datetime.strptime(ts[:10], "%Y-%m-%d")
     local_tz = pytz.timezone("America/Santiago")
-    date_from = local_tz.localize(execution_date).astimezone(pytz.utc)
-    date_to = date_from + timedelta(days=1)
+    date_init = local_tz.localize(execution_date).astimezone(pytz.utc)
+    date_from = date_init - timedelta(hours=24)
+    date_to = date_init
 
     print(date_from)
     print(date_to)
